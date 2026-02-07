184cf225f7ffcb97e5fde64bbd9d535ec19a1284
Andrés Guerrero <202307281@alu.comillas.edu>
2026-02-07 20:29:07 +0100

    Primer commit

---
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/andresguerrero/AndresguerreroApplication.java
@@ -0,0 +1,13 @@
+package edu.comillas.icai.gitt.pat.spring.andresguerrero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndresguerreroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndresguerreroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/comillas/icai/gitt/pat/spring/andresguerrero/AndresguerreroApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.comillas.icai.gitt.pat.spring.andresguerrero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndresguerreroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
